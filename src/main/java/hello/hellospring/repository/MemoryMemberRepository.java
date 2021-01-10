fd8056c14551e81ada4c77c5ed9c3561fdf014ac
skypotato <hunsooski@naver.com>
2021-01-10 15:57:05 +0900

    2021.01.09 : 회원 도메인과 테스트 케이스 작성 - #3

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -28,7 +28,11 @@ public class MemoryMemberRepository implements MemberRepository {
     }
 
     @Override
-    public List<Member> finAll() {
+    public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
