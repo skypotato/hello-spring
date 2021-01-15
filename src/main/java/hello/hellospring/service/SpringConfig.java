0c9fb6786f69deb53b1704d38cc7cf856504ce63
skypotato <hunsooski@naver.com>
2021-01-15 22:25:24 +0900

    2021.01.14 : 스프링 의존관계 설정하는 다른 방식 #4

---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,19 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(meberRepository());
+    }
+
+    @Bean
+    public MemberRepository meberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
