b9847ce46ac773a70b8a8bf1a181e584897fe049
skypotato <hunsooski@naver.com>
2021-01-17 21:18:00 +0900

    2021.01.14 : 회원 웹 기능 등록 #5

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,6 +13,11 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/member/new")
+    public String createForm(){
+        return "member/createMemberForm";
     }
 
 }
