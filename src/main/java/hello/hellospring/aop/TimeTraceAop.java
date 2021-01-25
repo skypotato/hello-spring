6e244194f64a2c02c9986a2098b4ada9804e4691
skypotato <hunsooski@naver.com>
2021-01-25 21:39:45 +0900

    2021.01.25 : AOP closed #7

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -3,8 +3,10 @@ package hello.hellospring.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class TimeTraceAop {
 
     @Around("execution(* hello.hellospring..*(..))")
--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -22,10 +22,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
-    @Bean
-    public TimeTraceAop timeTraceAop() {
-        return new TimeTraceAop();
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository meberRepository() {
 ////        return new MemoryMemberRepository();
